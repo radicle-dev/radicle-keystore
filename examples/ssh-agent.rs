7517325791f7d7a105dd5861e45b473f6ac4874e
xphoniex <xphoniex@users.noreply.github.com>
2022-10-10 11:31:11 +0000

    Connect to ssh-agent in a sync way
    Signed-off-by: xphoniex <xphoniex@users.noreply.github.com>

--- examples/ssh-agent.rs
+++ examples/ssh-agent.rs
@@ -16,56 +16,46 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 #[cfg(feature = "ssh-agent")]
-use smol::{io, net::unix::UnixStream};
+use std::{io, os::unix::net::UnixStream};
 
 #[cfg(feature = "ssh-agent")]
 fn main() -> io::Result<()> {
     use radicle_keystore::sign::{ssh, Signer, SshAgent};
     use rand::rngs::OsRng;
 
-    smol::block_on(async {
-        let sk = ed25519_zebra::SigningKey::new(OsRng {});
-        let pk = ed25519_zebra::VerificationKey::from(&sk);
-        let public = ssh::ed25519::PublicKey(pk.into());
-        let agent = SshAgent::new(public);
+    let sk = ed25519_zebra::SigningKey::new(OsRng {});
+    let pk = ed25519_zebra::VerificationKey::from(&sk);
+    let public = ssh::ed25519::PublicKey(pk.into());
+    let agent = SshAgent::new(public);
 
-        // This could be a `rad-ssh-add` executable which reads the local key from
-        // the filestore (prompting for the password).
-        ssh::add_key::<UnixStream>(&agent, sk, &[]).await.unwrap();
+    // This could be a `rad-ssh-add` executable which reads the local key from
+    // the filestore (prompting for the password).
+    ssh::add_key::<UnixStream>(&agent, sk, &[]).unwrap();
 
-        println!("connecting to ssh-agent");
-        let signer = agent
-            .connect::<UnixStream>()
-            .await
-            .expect("could not connect to ssh-agent");
-        println!("asking agent to sign some data");
-        let sig = signer
-            .sign(b"cooper")
-            .await
-            .expect("signing via ssh-agent failed");
-        println!("verifying signature");
-        pk.verify(&ed25519_zebra::Signature::from(sig.0), b"cooper")
-            .expect("ssh-agent didn't return a valid signature");
-        println!("it worksed");
+    println!("connecting to ssh-agent");
+    let signer = agent
+        .connect::<UnixStream>()
+        .expect("could not connect to ssh-agent");
+    println!("asking agent to sign some data");
+    let sig = signer
+        .sign(b"cooper")
+        .expect("signing via ssh-agent failed");
+    println!("verifying signature");
+    pk.verify(&ed25519_zebra::Signature::from(sig.0), b"cooper")
+        .expect("ssh-agent didn't return a valid signature");
+    println!("it worksed");
 
-        let keys = ssh::list_keys::<UnixStream>(&agent)
-            .await
-            .expect("could not list keys");
-        if keys.contains(&public) {
-            println!("added key succesfully")
-        }
-        ssh::remove_key::<UnixStream>(&agent, &public)
-            .await
-            .expect("could not remove key from ssh-agent");
-        let keys = ssh::list_keys::<UnixStream>(&agent)
-            .await
-            .expect("could not list keys");
-        if !keys.contains(&public) {
-            println!("removed key successfully")
-        }
+    let keys = ssh::list_keys::<UnixStream>(&agent).expect("could not list keys");
+    if keys.contains(&public) {
+        println!("added key succesfully")
+    }
+    ssh::remove_key::<UnixStream>(&agent, &public).expect("could not remove key from ssh-agent");
+    let keys = ssh::list_keys::<UnixStream>(&agent).expect("could not list keys");
+    if !keys.contains(&public) {
+        println!("removed key successfully")
+    }
 
-        Ok(())
-    })
+    Ok(())
 }
 
 #[cfg(not(feature = "ssh-agent"))]
